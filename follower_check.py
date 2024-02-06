4aacaa09392266bd234c20319b689160f766d2e2
Linjia Zhou <44042122+Linjia-Zhou@users.noreply.github.com>
2024-02-05 22:15:31 -0500

    Update follower_check.py

--- follower_check.py
+++ follower_check.py
@@ -3,22 +3,29 @@ import openpyxl
 
 """
 INSTRUCTIONS:
+Request to download Instagram information from: https://accountscenter.instagram.com/info_and_permissions/
 In Excel,
-1. Import html data into a blank excel file --> command + A shortcut to highlight entire webpage
-2. Make sure first row (A1) says "Follow" for both files. 
-    This will be the column name and needs to be the same
+- Copy HTML data into a blank Excel file --> command + A shortcut to highlight entire webpage
+
 In Python,
-3. Insert file location into following and followers variables
+- Insert file location into "following" and "followers" variables
+
+TO VIEW WHO DOESN'T FOLLOW YOU BACK:
+- uncomment print statement 
 """
 # INSERT EXCEL FILES HERE
 following = pd.read_excel('')
 followers = pd.read_excel('')
 
+#delete the blankspace
+followers = followers.drop([0, 1, 2])
+following = following.drop([0, 1, 2, 3])
+
 followers = followers.iloc[::2]  # deleting every other row
 following = following.iloc[::2]
 
-followers_sorted = followers.sort_values('Follow')  # alphabetical order
-following_sorted = following.sort_values('Follow')  # alphabetical order
+followers_sorted = followers.sort_values(by='Unnamed: 0')  # alphabetical order
+following_sorted = following.sort_values(by='Unnamed: 0')  # alphabetical order
 
 diff_rows = following.merge(followers, how = 'outer' ,indicator=True)
 
